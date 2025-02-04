51105ce903bcf52f657b99d39d709a80571a16d9
Vitor Andreola <dev.andreola@gmail.com>
2025-02-03 22:17:01 -0300

    feat: update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,44 +6,32 @@ export default function Footer() {
       <div className="container mx-auto px-6">
         <div className="flex flex-wrap justify-between">
           <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-semibold mb-2">FrontEnd Toolkit</h3>
+            <h3 className="text-lg font-semibold mb-2 tracking-tighter">
+              Front<span className="font-light">Sheet</span>
+            </h3>
             <p className="text-zinc-400">
-              Empowering web developers with essential tools and resources.
+              Capacitando desenvolvedores web com ferramentas e recursos
+              essenciais.
             </p>
           </div>
           <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h4 className="text-lg font-semibold mb-2">Quick Links</h4>
+            <h4 className="text-lg font-semibold mb-2">Acesso rápido</h4>
             <ul className="space-y-2">
               <li>
                 <Link href="#" className="text-zinc-400 hover:text-white">
-                  Home
+                  Início
                 </Link>
               </li>
               <li>
                 <Link href="#" className="text-zinc-400 hover:text-white">
-                  Tools
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="text-zinc-400 hover:text-white">
-                  Resources
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="text-zinc-400 hover:text-white">
-                  About
+                  Máscaras
                 </Link>
               </li>
             </ul>
           </div>
           <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h4 className="text-lg font-semibold mb-2">Connect</h4>
+            <h4 className="text-lg font-semibold mb-2">vt-code</h4>
             <ul className="space-y-2">
-              <li>
-                <Link href="#" className="text-zinc-400 hover:text-white">
-                  Twitter
-                </Link>
-              </li>
               <li>
                 <Link href="#" className="text-zinc-400 hover:text-white">
                   GitHub
@@ -59,20 +47,17 @@ export default function Footer() {
           <div className="w-full md:w-1/4">
             <h4 className="text-lg font-semibold mb-2">Newsletter</h4>
             <p className="text-zinc-400 mb-2">
-              Stay updated with our latest tools and resources
+              Mantenha-se atualizado com nossas últimas ferramentas e recursos
             </p>
             <input
               type="email"
-              placeholder="Enter your email"
+              placeholder="Insira seu e-mail"
               className="w-full p-2 rounded bg-zinc-700 text-white"
             />
           </div>
         </div>
         <div className="mt-8 text-center text-zinc-400">
-          <p>
-            &copy; {new Date().getFullYear()} FrontEnd Toolkit. All rights
-            reserved.
-          </p>
+          <p>&copy; {new Date().getFullYear()} vt-code. All rights reserved.</p>
         </div>
       </div>
     </footer>
